4eb107f1d233bb4c755c5753167ae3849bee5f55
MEEEXIM <maxim-malyshev.2010@mail.ru>
2023-12-23 19:31:24 +0700

    dz

--- class.py
+++ class.py
@@ -12,14 +12,47 @@ class cars:
 car1 = cars('Russia', '1stmodel', 1999, 130)
 car2 = cars('USA', '2nd model', 2015, 160)
 car3 = cars('Russia', '2ndmodel', 2021, 250)
-b = int(input('Какой  автомобиль?'))
-a = int(input('Какое растояние?'))
-if b == 1:
-    car1.info()
-    car1.T(a)
-elif b == 2:
-    car2.info()
-    car2.T(a)
-else:
-    car3.info()
-    car3.T(a)
+# b = int(input('Какой  автомобиль?'))
+# a = int(input('Какое растояние?'))
+# if b == 1:
+#     car1.info()
+#     car1.T(a)
+# elif b == 2:
+#     car2.info()
+#     car2.T(a)
+# else:
+#     car3.info()
+#     car3.T(a)
+
+
+
+
+
+
+
+
+# номер 1
+class calcul:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def delenie(self):
+        print(self.a / self.b)
+    def umnogenie(self):
+        print(self.a * self.b)
+    def vichitanie(self):
+        print(self.a - self.b)
+    def slogenie(self):
+        print(self.a + self.b)
+c=''
+while c != '==':
+    c=input()
+    f=calcul(int(input()), int(input()))
+    if c == '/':
+        f.delenie()
+    elif c == '*':
+        f.umnogenie()
+    elif c == '-':
+        f.vichitanie()
+    else:
+        f.slogenie()
